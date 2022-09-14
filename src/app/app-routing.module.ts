ad1c3d91649dab2513f1390f08907524f72fb4d4
Gabriela Gastaldi <gabitag2003@gmail.com>
2022-09-14 18:29:09 -0300

    Cambios en el maqueteado frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,3 +8,14 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+// import { NgCircleProgressModule } from 'ng-circle-progress';
+// @NgModule({
+//   imports:  NgCircleProgressModule.forRoot({})],
+ 
+ 
+// //veras que ahi esta el NgCircle.  Tambien ten en cuenta de que tu app-component.html tenga esta etiqueta:  <router-outlet></router-outlet>
+// Sino te funciona escribeme por email. y entramos en un zoom luisfelipe782006@gmail
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
